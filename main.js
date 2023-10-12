c4adf37a76d92649e86f12d19315cc802ebfdf40
samirfaritov <samirfaritov6@gmail.com>
2023-10-12 15:09:16 +0500

    js11 uy ishi

---
+++ main.js
@@ -0,0 +1,18 @@
+let a = "100"
+let pr = prompt("100")
+
+// if (a >= pr) {
+//     console.log("Yaxhi");
+// } else {
+//     prompt(100)
+// }
+
+for(let i = 0; i < a.length; i++) {
+    if(a === pr) {
+        console.log(true);
+    } else {
+        prompt(100)
+    }
+} 
+
+// console.log(i);
